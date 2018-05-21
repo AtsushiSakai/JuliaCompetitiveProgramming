eb36eb0060190070f0cc6ffcd711097b15072fbc
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-05-21 20:15:47 +0900

    improve code

--- 5/spam.jl
+++ 5/spam.jl
@@ -1,3 +1,3 @@
 data = 1:10
-count = length(filter(x-> x%3==0, data))
+count = length([x for x in data if x%3 == 0])
 println(count)
--- 8/spam.jl
+++ 8/spam.jl
@@ -1,9 +1,20 @@
+# 二次元の配列の場合
 data = [1 2 3
         4 5 6
         7 8 9]
 show(STDOUT, "text/plain", data);println()
 
-flat = [flatten for inner in data for flatten in inner]
+flat = vec(data) #ベクトル化
 
 show(STDOUT, "text/plain", flat);println()
+
+# 二重の配列の場合
+data2 = [[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+show(STDOUT, "text/plain", data2);println()
+
+flat2 = [x for inner in data for x in inner]
+show(STDOUT, "text/plain", flat2);println()
+
 
